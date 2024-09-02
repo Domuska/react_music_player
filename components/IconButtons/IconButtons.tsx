8a63ecd2e2e4b577f90d5afbd559471f221735c6
Tomi Lämsä <tomi.lamsa@gofore.com>
2024-09-02 20:31:05 +0300

    Add volume control seekbar & mute button & functionality

--- components/IconButtons/IconButtons.tsx
+++ components/IconButtons/IconButtons.tsx
@@ -92,3 +92,35 @@ export const PauseButton = ({ onClick }: { onClick?: VoidFunction }) => {
     </StyledButton>
   );
 };
+
+export const VolumeDownButton = ({ onClick }: { onClick?: VoidFunction }) => {
+  return (
+    <StyledButton onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="#5f6368"
+      >
+        <path d="M200-360v-240h160l200-200v640L360-360H200Zm440 40v-322q45 21 72.5 65t27.5 97q0 53-27.5 96T640-320ZM480-606l-86 86H280v80h114l86 86v-252ZM380-480Z" />
+      </svg>
+    </StyledButton>
+  );
+};
+
+export const MutedButton = ({ onClick }: { onClick?: VoidFunction }) => {
+  return (
+    <StyledButton onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="#5f6368"
+      >
+        <path d="M792-56 671-177q-25 16-53 27.5T560-131v-82q14-5 27.5-10t25.5-12L480-368v208L280-360H120v-240h128L56-792l56-56 736 736-56 56Zm-8-232-58-58q17-31 25.5-65t8.5-70q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 53-14.5 102T784-288ZM650-422l-90-90v-130q47 22 73.5 66t26.5 96q0 15-2.5 29.5T650-422ZM480-592 376-696l104-104v208Zm-80 238v-94l-72-72H200v80h114l86 86Zm-36-130Z" />
+      </svg>
+    </StyledButton>
+  );
+};
